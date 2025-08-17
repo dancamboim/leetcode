7f92155df8a35ed1da99c4085e754f871c220dbc
Daniel Camboim <danielcamboim147159@gmail.com>
2025-08-17 15:33:22 -0300

    Includes PalindromeNumber problem

---
+++ PalindromeNumber.cs
@@ -0,0 +1,32 @@
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+// Example 1:
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+// Follow up: Could you solve it without converting the integer to a string?
+
+using System.Linq;
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        
+         var numberString = x.ToString().ToCharArray();
+        var numberStringReverse = (char[])numberString.Clone();
+        
+        Array.Reverse(numberStringReverse);
+
+        return numberString.SequenceEqual(numberStringReverse);
+    }
+}
