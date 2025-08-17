0eec6112363d23734b15e93049061daa30472f5d
Daniel Camboim <danielcamboim147159@gmail.com>
2025-08-17 16:38:55 -0300

    Includes alternative solutions for PalindromeNumber problem

--- PalindromeNumber.cs
+++ PalindromeNumber.cs
@@ -19,13 +19,80 @@
 
 public class Solution
 {
-    public bool IsPalindrome(int x)
+    public bool IsPalindromeComparingReverseArray(int x)
     {
+        //Runtime: 6 ms - beats 22.32%
+        //memory: 38.85mb ms - beats 17.72%
+
         var numberString = x.ToString().ToCharArray();
         var numberStringReverse = (char[])numberString.Clone();
 
         Array.Reverse(numberStringReverse);
 
         return numberString.SequenceEqual(numberStringReverse);
+    }
+
+    //Runtime 8ms Beats 17.13% =(
+    //Memory 37.34mb Beats 27.38% =(
+    public bool IsPalindromeNoConvertingToCharArray(int x)
+    {
+        //A negative number will never be a palindrome!
+        if (x < 0) return false;
+
+
+        //Log10(x) is undefined for negative numbers!
+        // Math.Log10(12345) ≈ 4.09 → floor = 4 → +1 → 5 digits
+        // If number == 0, this formula breaks
+        if (x == 0) return true;
+        int xLength = (int)Math.Floor(Math.Log10(x)) + 1;
+
+        int[] xArray = new int[xLength];
+        var xAux = x;
+
+        for (var i = xLength - 1; i >= 0; i--)
+        { //will iterate starting from the end, that's why i--
+            xArray[i] = xAux % 10; // % 10 → gets the last digit (like 5 from 12345)
+            xAux /= 10; //removes the last digit (12345 → 1234 → ...)
+        }
+
+        xAux = x;
+        int[] xReversed = new int[xLength];
+        for (var i = 0; i < xLength; i++) //will iterate starting from the initial position in the array, that's why i++
+        {
+            xReversed[i] = xAux % 10;
+            xAux /= 10;
+        }
+
+        return xArray.SequenceEqual(xReversed);
+    }
+
+    public bool IsPalindromeNoConvertingToCharArrayUsingOneArrayOnly(int x)
+    {
+
+        //A negative number will never be a palindrome!
+        if (x < 0) return false;
+
+        //Log10(x) is undefined for negative numbers!
+        // Math.Log10(12345) ≈ 4.09 → floor = 4 → +1 → 5 digits
+        // If number == 0, this formula breaks
+        if (x == 0) return true;
+        int xLength = (int)Math.Floor(Math.Log10(x)) + 1;
+
+        int[] xArray = new int[xLength];
+
+        //will iterate starting from the end, that's why i--
+        for (var i = xLength - 1; i >= 0; i--)
+        {
+            xArray[i] = x % 10; // % 10 → gets the last digit (like 5 from 12345)
+            x /= 10; //removes the last digit (12345 → 1234 → ...)
+        }
+
+        //Will iterate i < xLength / 2 because it has only to compare to the other half of the array
+        for (var i = 0; i < xLength / 2; i++)
+        {
+            if (xArray[i] != xArray[xLength - 1 - i]) return false;
+        }
+        return true;
+
     }
 }
